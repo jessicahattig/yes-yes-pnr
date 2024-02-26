997b7e37e6e39ddedc5d3db74b430df470846445
jessicahattig <jessicahattig@gmail.com>
2024-02-25 16:45:52 -0800

    WIP: Home component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,8 +17,7 @@ function Home() {
           <Carousel.Caption>
           <h2>Secure Funding to Replace Aging Community Pool</h2>
           <p>Each year, 1,500+ children participate in swim lessons.</p>
-          <p>The pool pipes and foundation, aged 76 years, urgently need updates. The pool tent cover has surpassed its expected lifespan by 7-10 years.</p> 
-          
+          <p>The pool pipes and foundation, aged 76 years, urgently need updates. The pool tent cover has surpassed its expected lifespan by 7-10 years.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -42,26 +41,14 @@ function Home() {
           />
           <Carousel.Caption>
             <div className='slide3'>
-            <h2>Obtain financing to build a new pool with community center.</h2>
-            <p>Over 3,200 individuals, including both youth and adults, enrolled in HRVPRD recreation programs in 2023.</p>
+            <h2>Help protect the natural environment.</h2>
+            <p>34 acres of parkland secured since 2020 for preservation, development, and trail connections. Say yes to securing more.</p>
             </div>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
       </div>
-    
   );
 }
 
 export default Home;
- {/* <ul>
-    <li>Capital Bond to build a state-of-the-art pool with a community center</li>
-    <li>Funding for the enhancement of parks and trails</li>
-    <li>Supported by Operation Levy, ensuring continued support for:</li>
-    <ul>
-      <li>Recreation programming</li>
-      <li>Park services</li>
-      <li>Recreation facilities</li>
-    </ul>
-  </ul>
-  <p>Together, these measures will create a vibrant and thriving community space for everyone.</p> */}
