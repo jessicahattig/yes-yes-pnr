599a26ed361eb0883f4ddea6601884ea5700d77d
jessicahattig <jessicahattig@gmail.com>
2024-03-03 14:30:08 -0800

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,32 @@
 import React from 'react';
 
 const Contact = () => {
-  const recipientEmail = 'jessicahattig@gmail.com'; 
+  const recipientEmail = 'jessicahattig@gmail.com'; // Replace with PAC email address.
+
   const handleEmailLinkClick = () => {
     window.location.href = `mailto:${recipientEmail}`;
   };
 
   return (
     <div className="ComponentHeader">
-      <p>
-        If you have any questions or concerns, please{' '}
-        <span style={{ color: 'blue', cursor: 'pointer' }} onClick={handleEmailLinkClick}>
-          contact us
-        </span>
-        .
-      </p>
+      <div className="ContactContainer">
+        <div className="TextContainer">
+          <p>
+            If you have any questions or concerns, please{' '}
+            <span className="EmailLink" onClick={handleEmailLinkClick}>
+              contact us
+            </span>
+            .
+          </p>
+        </div>
+        <div className="ImageContainer">
+          <img
+            src="./soccer.png"
+            alt="Contact Us"
+            className="ContactImage"
+          />
+        </div>
+      </div>
     </div>
   );
 };
