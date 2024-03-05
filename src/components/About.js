948f90ebe969c3a16ff5e115a95f1e22948dca61
jessicahattig <jessicahattig@gmail.com>
2024-03-05 00:21:19 -0800

    WIP: Donate.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,36 +7,64 @@ const About = () => {
         <div className="col-md-6">
         <h1 className="mb-4 custom-heading">Operational Levy</h1>
         <h4 className="mb-4 custom-heading">To Build Pool, Parks, & Trails</h4>
-          <p className="lead">For the sustained operation and maintenance of Hood River Valley Parks and Recreation District (HRVPRD), we propose an Operational Levy. This levy, with a length of 5 years and a tax rate of $0.43 per $1,000 assessed value, ensures the continuity of essential services. The estimated annual cost for the average household is $94, contributing to the care of public resources, increased maintenance demands, and the operation of an expanded pool within a community center.</p>
+        <div className='lead'>
+        <ul>
+        <p><strong>New Pool with Community Center & Recreation Space.</strong> To include modern amenities such as: warm water instruction and therapy pool, competition pool, family changing areas, a multipurpose workout room, and classroom space for community use & recreation programing.</p>
+        <p><strong>Develop Westside Park,</strong> including new multi-purpose field for softball, baseball, soccer, & lacrosse, picnicking, trails, & a natural park on 20 acres on Fairview and Belmont (near Westside School).</p>
+        <p><strong>Purchase & Preserve Parkdale Park</strong> as a community park. This land is zoned commercial & privately owned. HRVPRD has an exclusive option to purchase the property.</p>
+        <p><strong>Construct Key Trail Connections.</strong> Connect the missing links on The Indian Creek Trail & Westside Community Trail to create walking access from downtown to Barrett Dr. & from Westside School to the Skate Park.</p>
+        <p>2023: HRVPRD Board considers multiple
+          packages and funding measures to address
+          community needs. (Pool with community center,
+          trails, park improvements, recreation).</p>
+        <br />
+<p><strong>Length:</strong> 25 Years</p>
+<p><strong>Tax Rate:</strong> $1.27per $1,000 Assessed Value</p>
+<p><strong>Estimated Annual Cost for Average Household:</strong> $279</p>
+      </ul>
+          </div>
         </div>
 
         <div className="col-md-6">
           <h1 className="mb-4 custom-heading">General Obligation Bond</h1>
           <h4 className="mb-4 custom-heading">For Operations & Maintenance</h4>
-          <p className="lead">In addition to the Operational Levy, we seek support for a General Obligation Bond. This 25-year initiative, with a tax rate of $1.27 per $1,000 assessed value, aims to fund significant projects, including the construction of a new pool with a community center and recreation space. The bond will also facilitate the development of Westside Park, the purchase and preservation of Parkdale Park, and the construction of key trail connections. The estimated annual cost for the average household is $279, contributing to the enhancement of recreational facilities and the overall well-being of the community.</p>
+          <div className="lead">
+          <ul>
+        <p><strong>Take Care of Public Resources.</strong> HRVPRD has one full-time maintenance employee who works out of a 10’x14’ shed in the aquatic center parking lot. With a new recreation center, and more parks & trails, more maintenance will be needed.</p>
+        <p><strong>Operate an Expanded Pool with Community
+Center.</strong> The new pool will be bigger with more amenities and need more supplies, utilities, chemicals, & staff (lifeguards, front
+desk).</p>
+        <p><strong>PProvide recreation opportunities for youth
+& adults.</strong>In 2021 HRVPRD entered an agreement with the School District to transfer community education (Recreation Programing) to the district. That agreement funded two employees through Sept 24.</p>
+<br />
+<p><strong>Length:</strong> 5 Years (will need renewal)</p>
+<p><strong>Tax Rate:</strong> $0.43 per $1,000 Assessed Value</p>
+<p><strong>Estimated Annual Cost for Average Household:</strong> $94</p>
+      </ul>
+            </div>
         </div>
       </div>
       <h2>Community Engagement & Solution Finding</h2>
       <ul>
-        <p>2017: Pool Study found the pool had 3-5 years
-          left of useful life & gave options for replacement.</p>
-        <p>2020: Parks Master Plan created & accepted by
+        <li>2017: Pool Study found the pool had 3-5 years
+          left of useful life & gave options for replacement.</li>
+        <li>2020: Parks Master Plan created & accepted by
           HRVPRD, City, County, Port, & School District,
-          with significant community input.</p>
-        <p>2021 Westside Park Study determined whether
+          with significant community input.</li>
+        <li>2021 Westside Park Study determined whether
           land was suitable for a park. Results of study &
           feedback led HRVPRD board to purchase the 20
-          acres identified in Parks Master Plan for future park development.</p>
-        <p>2017-2023 - Extensive Community Input
+          acres identified in Parks Master Plan for future park development.</li>
+        <li>2017-2023 - Extensive Community Input
           received through stakeholder interviews,
           multiple English & Spanish focus groups, tabling
           events, local group meetings, online surveys,
-          open houses, & two statistically valid surveys.</p>
-        <p>2023: HRVPRD Board considers multiple
+          open houses, & two statistically valid surveys.</li>
+        <li>2023: HRVPRD Board considers multiple
           packages and funding measures to address
           community needs. (Pool with community center,
-          trails, park improvements, recreation).</p>
-        <p>Combination of two ballot measures are referred to voters or the May 2024 election.</p>
+          trails, park improvements, recreation).</li>
+        <li>Combination of two ballot measures are referred to voters or the May 2024 election.</li>
       </ul>
     </div>
   );
--- src/components/Donate.js
+++ src/components/Donate.js
@@ -5,6 +5,12 @@ function Donate() {
   return (
     <div className="ComponentHeader">
       <h2>Donation Page is Under Construction.</h2>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       </div>
   );
 }
