a78e4a06c78d079b804a85563955ba85cf7beca2
jessicahattig <jessicahattig@gmail.com>
2024-03-10 10:52:14 -0700

    Add input component. WIP: firebase db.

---
+++ src/components/Input.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Input(props){
+
+  return (
+    <React.Fragment>
+      <div onClick = {() => props.whenInputClicked(props.id)}>
+        <h3>{props.location} - {props.names}</h3>
+        <p><em>{props.issue}</em></p>
+        <p><em>{props.formattedWaitTime}</em></p>
+        <hr/>
+      </div>
+    </React.Fragment>
+  );
+}
+
+Input.propTypes = {
+  names: PropTypes.string,
+  location: PropTypes.string,
+  issue: PropTypes.string,
+  formattedWaitTime: PropTypes.string,
+  id: PropTypes.string,
+  whenInputClicked: PropTypes.func
+}
+
+export default Input;
--- src/components/PageControl.js
+++ src/components/PageControl.js
@@ -2,16 +2,60 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import About from './About';
 import Contact from './Contact';
-import InputForm from './InputForm';
+import NewInputForm from './NewInputForm';
 import Home from './Home';
 import PacAbout from './PacAbout';
-
+import { collection, addDoc } from "firebase/firebase";
 
 function PageControl() {
+
+  const [ formVisibleOnPage, setFormVisibleOnPage ] = useState(false);
+  // const [ mainTicketList, setMainTicketList ] = useState([]);
+  // const [ selectedTicket, setSelectedTicket ] = useState(null);
+  // const [ editing, setEditing ] = useState(false);
+  // const [ error, setError ] = useState(null);
+
+  useEffect(() => {
+    // const queryByTimestamp = query(
+    //   collection(db, "inputs"), 
+    //   // orderBy('timeOpen')
+    // );
+    const unSubscribe = onSnapshot(
+      queryByTimestamp, 
+      (querySnapshot) => {
+        const tickets = [];
+        querySnapshot.forEach((doc) => {
+          const timeOpen = doc.get('timeOpen', {serverTimestamps: "estimate"}).toDate();
+          const jsDate = new Date(timeOpen);
+          tickets.push({
+            names: doc.data().names, 
+            location: doc.data().location, 
+            issue: doc.data().issue, 
+            timeOpen: jsDate,
+            formattedWaitTime: formatDistanceToNow(jsDate),
+            id: doc.id
+          });
+        });
+        setMainTicketList(tickets);
+      },
+      (error) => {
+        setError(error.message);
+      }
+    );
+
+    return () => unSubscribe();
+  }, []);
+
+
+  const handleAddingNewInput = async (newInputData) => {
+    await addDoc(collection(db, "inputs"), newInputData);
+    setFormVisibleOnPage(false);
+  }
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/input-form" element={<InputForm />} />
+      <Route path="/new-input-form" element={<NewInputForm />} />
       <Route path="/about" element={<About />} />
       <Route path="/pac-about" element={<PacAbout />} />
       <Route path="/contact" element={<Contact />} />
